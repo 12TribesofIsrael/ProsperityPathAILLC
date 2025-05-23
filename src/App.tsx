47e98779ce25c2a9e3081fa597493e267fe55a38
12TribesofIsrael <info@bornmadebosses.com>
2025-05-23 14:24:44 -0400

    Test: Simplified React app to diagnose blank page issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Pages
-import LandingPage from './pages/LandingPage';
-import LeadCapturePage from './pages/LeadCapturePage';
-import ThankYouPage from './pages/ThankYouPage';
-import AboutPage from './pages/AboutPage';
-import NotFoundPage from './pages/NotFoundPage';
-
-// Components
-import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <Router basename="/ProsperityPathAILLC">
-      <Routes>
-        <Route path="/" element={<Layout><LandingPage /></Layout>} />
-        <Route path="/opt-in" element={<Layout><LeadCapturePage /></Layout>} />
-        <Route path="/thank-you" element={<Layout><ThankYouPage /></Layout>} />
-        <Route path="/about" element={<Layout><AboutPage /></Layout>} />
-        <Route path="*" element={<Layout><NotFoundPage /></Layout>} />
-      </Routes>
-    </Router>
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Prosperity Path AI - Test Page</h1>
+      <p>If you can see this, the React app is working!</p>
+      <p>Site URL: https://12tribesofisrael.github.io/ProsperityPathAILLC/</p>
+    </div>
   );
 }
 
